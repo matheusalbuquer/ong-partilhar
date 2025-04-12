d6906eb7a17e2b08d5e7f607990710541aad1e5e
Luan Vinicius <luanllacraft@gmail.com>
2025-04-12 11:40:56 -0300

    Adicionado itens ao bazar

---
+++ bazar/bazar.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    const produtos = [
+        {nome: 'Vetido preto gliter', imagem:'./imgs/vestidoGliter.png', preco: 20},
+        {nome: "Vestido florido", imagem:"./imgs/vestidoFlorido.png",preco: 20},
+        {nome: "Blusa branca com bolinhas", imagem:"./imgs/blusaBrancaBolinhas.png",preco: 20},
+        {nome: "Vestido florido com manga",imagem:"./imgs/vestidoFloridoManga.png",preco: 20},
+        {nome: "Blusa adidas vermelha",imagem:"./imgs/blusaAdidas.png",preco: 15},
+        {nome: "Blusa estampada",imagem:"./imgs/blusaEstampada.png",preco: 20},
+        {nome: "Macacão azul marinho",imagem:"./imgs/macacao.png",preco: 35},
+        {nome: "Conjunto blusa e short",imagem:"./imgs/Conjunto.png",preco: 30}
+        ];
+
+        const produtosBazar = document.querySelector(".conteiner-main-bazar")
+
+        produtos.forEach (produtosLista => {
+            const produtosCard = document.createElement('div')
+            produtosCard.classList.add('produto-card')
+
+            produtosCard.innerHTML = `
+
+                <img src="${produtosLista.imagem}" alt="${produtosLista.nome}">
+                <p>${produtosLista.nome}</p>
+                <span>R$ ${produtosLista.preco},00</span>
+                 <a href="">
+                    Quero esta peça
+                </a>
+            `
+
+            produtosBazar.appendChild(produtosCard)
+
+        })
+});
+    
+
+
+
+
+
+
